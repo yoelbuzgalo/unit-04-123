821d7624fce04ddf2fe5005993ac18f9b976bf51
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-09-20 11:50:35 -0400

    Activity 4.9

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -6,6 +6,15 @@ def encrypt_letter(letter, shift):
         return new_letter # Return the converted letter
     else:
         return ''
+    
+def decrypt_letter(letter, shift):
+    ascii_key = ord(letter)# Convert letter to ascii using ord
+    shifted_ascii = ascii_key - shift # Shift new letter by subtracting shift value to the ascii key
+    decrypted_letter = chr(shifted_ascii) # Convert the new letter ascii key back to character form
+    if is_alphabetic(decrypted_letter):
+        return decrypted_letter
+    else:
+        return ''
 
 def is_alphabetic(character):
     return (character >= "A" and character <= "Z")
--- caesar_cipher_test.py
+++ caesar_cipher_test.py
@@ -70,3 +70,50 @@ def test_encrypt_letter_nonalphabetic():
     # Analyze
     assert result == expected
 
+def test_decrypt_letter_A_3():
+    # Setup
+    letter = "D"
+    shift = 3
+    expected = "A"
+
+    # Invoke
+    result = caesar_cipher.decrypt_letter(letter, shift)
+
+    # Analyze
+    assert result == expected
+
+def test_decrypt_letter_G_5():
+    # Setup
+    letter = "L"
+    shift = 5
+    expected = "G"
+
+    # Invoke
+    result = caesar_cipher.decrypt_letter(letter, shift)
+
+    # Analyze
+    assert result == expected
+
+def test_decrypt_letter_D_10():
+    # Setup
+    letter = "N"
+    shift = 10
+    expected = "D"
+
+    # Invoke
+    result = caesar_cipher.decrypt_letter(letter, shift)
+
+    # Analyze
+    assert result == expected
+
+def test_decrypt_letter_nonalphabetic():
+    # Setup
+    letter = "*"
+    shift = 3
+    expected = ''
+
+    # Invoke
+    result = caesar_cipher.decrypt_letter(letter,shift)
+
+    # Analyze
+    assert result == expected
