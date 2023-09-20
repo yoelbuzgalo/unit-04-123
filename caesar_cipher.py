6bd44b4500f5b9f5c248414aa032dc618d92fbdf
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-09-20 10:54:46 -0400

    Activity 4.3

---
+++ caesar_cipher.py
@@ -0,0 +1,5 @@
+def encrypt_letter(letter, shift):
+    # Convert letter to ascii using ord
+    # Shift new letter by adding shift value to the ascii key
+    # Convert the new letter ascii key back to character form
+    # Return the converted letter
