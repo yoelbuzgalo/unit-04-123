95c5ba44b3b5e727945d3fc93c06da5fdac4ef16
YoelBuzgalo <yoelbuzgalo@gmail.com>
2023-09-22 20:35:29 -0400

    Assignment 4.2 (Part 5)

--- random_password.py
+++ random_password.py
@@ -8,3 +8,42 @@ def create_ascii_range_string(start, stop):
         ascii_string += ascii_key
     return ascii_string
 
+def create_uppercase_letters_string():
+    '''
+    This function creates and returns a string between A to Z (upper case)
+    '''
+    # Calls create_ascii_range_string to create a string between A to Z and stores them in the variable uppercase_letters_string
+    uppercase_letters_string = create_ascii_range_string(65, 91)
+    return uppercase_letters_string # Returns the created uppercase letters string
+
+def create_lowercase_letters_string():
+    '''
+    This function creates and returns a string between a to z (lower case)
+    '''
+    # Calls create_ascii_range_string to create a string between a to z and stores them in the variable lowercase_letters_string
+    lowercase_letters_string = create_ascii_range_string(97, 123)
+    return lowercase_letters_string # Returns the created lowercase letters string
+
+def create_digits_string():
+    '''
+    This function creates and returns a string between 0 to 9 (digit only)
+    '''
+    # Calls create_ascii_range_string to create a string between 0 to 9 and stores them in the variable digits_string
+    digits_string = create_ascii_range_string(48, 58)
+    return digits_string # Returns the created digits string
+
+
+def create_symbols_string():
+    '''
+    This function creates and returns a string between ! to ~ (symbols only)
+    '''
+
+    # Calls for each range in ascii table that has symbols and stores them in variables
+    symbols_string_1 = create_ascii_range_string(33,48)
+    symbols_string_2 = create_ascii_range_string(58,65)
+    symbols_string_3 = create_ascii_range_string(91, 97)
+    symbols_string_4 = create_ascii_range_string(123,127)
+
+    # Concatate all of the symbol strings together
+    symbols_string = symbols_string_1+symbols_string_2+symbols_string_3+symbols_string_4
+    return symbols_string # Returns the created symbols string
--- test_random_password.py
+++ test_random_password.py
@@ -11,3 +11,43 @@ def test_create_ascii_range_string_97_100():
 
     # Analysis
     assert result == expected
+
+def test_create_uppercase_letters_string():
+    # Setup
+    expected = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    # Invoke
+    result = random_password.create_uppercase_letters_string()
+
+    # Analysis
+    assert result == expected
+
+def test_create_lowercase_letters_string():
+    # Setup
+    expected = "abcdefghijklmnopqrstuvwxyz"
+
+    # Invoke
+    result = random_password.create_lowercase_letters_string()
+
+    # Analysis
+    assert result == expected
+
+def test_create_digits_string():
+    # Setup
+    expected = "0123456789"
+
+    # Invoke
+    result = random_password.create_digits_string()
+
+    # Analysis
+    assert result == expected
+
+def test_create_symbols_string():
+    # Setup
+    expected = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+    # Invoke
+    result = random_password.create_symbols_string()
+
+    # Analysis
+    assert result == expected
